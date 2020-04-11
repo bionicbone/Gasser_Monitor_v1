826bd4f60841f715429312f0d856589d41f8c7a0
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2020-04-11 08:50:14 +0100

    Add temperature sensors
    Ambient, Canopy and Engine / ESC LM35 sensor code added
    Sent via Telemetry

---
+++ Temperature.cpp
@@ -0,0 +1,98 @@
+// 
+// 
+// 
+
+#include "Config.h"
+#include "Temperature.h"
+
+
+// Config
+const float AMBIENT_TEMP_CALIBRATION = -0.48;
+const float CANOPY_TEMP_CALIBRATION = -0.13;
+const float ENGINE_TEMP_CALIBRATION = 0.41;
+
+
+// Public Variables
+float ambientTemp = 0.00;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+float canopyTemp = 0.00;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+float engineTemp = 0.00;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+
+
+// Private Variables
+float			avgAmbientTemp = 0.00;
+float			avgCanopyTemp = 0.00;
+float			avgEngineTemp = 0.00;
+uint8_t		tempReadings = 0;
+bool			eepromUpdateRequired = false;
+
+
+void temperature_Setup() {
+	pinMode(PIN_AMBIENT_TEMPERATURE_LM35, INPUT);
+	pinMode(PIN_CANOPY_TEMPERATURE_LM35, INPUT);
+	pinMode(PIN_ENGINE_TEMPERATURE_LM35, INPUT);
+}
+
+void read_temperatures() {
+	// takes the average of 50 readings and populates ambientTemp, canopyTemp &  engineTemp global variables
+
+	avgAmbientTemp += (analogRead(PIN_AMBIENT_TEMPERATURE_LM35) * 3.3 / 4096) * 100 + AMBIENT_TEMP_CALIBRATION;
+	avgCanopyTemp += (analogRead(PIN_CANOPY_TEMPERATURE_LM35) * 3.3 / 4096) * 100 + CANOPY_TEMP_CALIBRATION;
+	avgEngineTemp += (analogRead(PIN_ENGINE_TEMPERATURE_LM35) * 3.3 / 4096) * 100 + ENGINE_TEMP_CALIBRATION;
+	tempReadings++;
+	if (tempReadings > TEMPERATURE_READINGS_FOR_AVERAGE) {
+		ambientTemp = avgAmbientTemp / tempReadings;
+		canopyTemp = avgCanopyTemp / tempReadings;
+		engineTemp = avgEngineTemp / tempReadings;
+		tempReadings = 0;
+		if (ambientTemp == AMBIENT_TEMP_CALIBRATION) ambientTemp = 0;
+		if (canopyTemp == CANOPY_TEMP_CALIBRATION) canopyTemp = 0;
+		if (engineTemp == ENGINE_TEMP_CALIBRATION) engineTemp = 0;
+		avgAmbientTemp = 0;
+		avgCanopyTemp = 0;
+		avgEngineTemp = 0;
+
+		// ********************************************************
+		// TODO - Activate code to write min / max values to EEPROM
+		// ********************************************************
+
+		//if (millis() > PROG_DELAY_BEFORE_FLIGHT_STAT_UPDATES) { // only perform updates once stable
+		//// update ambient temperature flight stats
+		//	if (ambientTemp < customFlightSatistics.MinAmbientTemperature) {
+		//		customFlightSatistics.MinAmbientTemperature = ambientTemp; eepromUpdateRequired = true;
+		//	}
+		//	else if (ambientTemp > customFlightSatistics.MaxAmbientTemperature) {
+		//		customFlightSatistics.MaxAmbientTemperature = ambientTemp; eepromUpdateRequired = true;
+		//	}
+
+		//	// update engine temperature flight stats
+		//	if (engineTemp < customFlightSatistics.MinEngineTemperature) {
+		//		customFlightSatistics.MinEngineTemperature = engineTemp; eepromUpdateRequired = true;
+		//	}
+		//	else if (engineTemp > customFlightSatistics.MaxEngineTemperature) {
+		//		customFlightSatistics.MaxEngineTemperature = engineTemp; eepromUpdateRequired = true;
+		//	}
+
+		//	// update canopy temperature flight stats
+		//	if (canopyTemp < customFlightSatistics.MinCanopyTemperature) {
+		//		customFlightSatistics.MinCanopyTemperature = canopyTemp; eepromUpdateRequired = true;
+		//	}
+		//	else if (canopyTemp > customFlightSatistics.MaxCanopyTemperature) {
+		//		customFlightSatistics.MaxCanopyTemperature = canopyTemp; eepromUpdateRequired = true;
+		//	}
+
+		//	// check if we need to update the EEPROM
+		//	if (eepromUpdateRequired == true) eeprom_WriteFilghtDataStats();
+		//}
+
+		// Sanity check the values and and set to zero if LM35 sensor is not attached
+		if (ambientTemp > 200) ambientTemp = 0;
+		if (canopyTemp > 200) canopyTemp = 0;
+		if (engineTemp > 200) engineTemp = 0;
+
+#if defined (DEBUG_LM35_TEMPERATURE_READINGS)
+		Serial.print("Ambient Temperature = "); Serial.print(ambientTemp); Serial.println("c");
+		Serial.print(" Canopy Temperature = "); Serial.print(canopyTemp); Serial.println("c");
+		Serial.print(" Engine Temperature = "); Serial.print(engineTemp); Serial.println("c");
+#endif
+	}
+}
---
+++ Temperature.h
@@ -0,0 +1,43 @@
+// Temperature.h
+
+#pragma once
+
+#ifndef _TEMPERATURE_h
+#define _TEMPERATURE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
+
+// Config
+constexpr auto TEMPERATURE_READINGS_FOR_AVERAGE					= 50;
+constexpr auto PROG_DELAY_BEFORE_FLIGHT_STAT_UPDATES		= 7500;		// if too low min/max values may not be stable. DEFAULT 7500;
+constexpr auto ERROR_MIN_CANOPY_TEMPERATURE = 2;
+constexpr auto ERROR_MAX_CANOPY_TEMPERATURE = 35;
+constexpr auto ERROR_MIN_ENGINE_TEMPERATURE = 2;
+constexpr auto ERROR_MAX_ENGINE_TEMPERATURE = 149;
+constexpr auto ERROR_MIN_AMBIENT_TEMPERATURE = 2;
+constexpr auto ERROR_MAX_AMBIENT_TEMPERATURE = 28;
+
+// Pins for the LM35 Temperature Probes
+#define PIN_CANOPY_TEMPERATURE_LM35				26						// Pin number for the canopy LM35 sensor
+#define PIN_AMBIENT_TEMPERATURE_LM35			27						// Pin number for the ambient LM35 sensor
+#define PIN_ENGINE_TEMPERATURE_LM35				28						// Pin number for the engine / ESC LM35 sensor
+ 
+
+// Public Variables
+extern float ambientTemp;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+extern float canopyTemp;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+extern float engineTemp;			// NOTE: These are float values on purpose so we can detect disconnected sensors
+
+// Public Functions
+void read_temperatures();
+void temperature_Setup();
+
+// Private Functions
