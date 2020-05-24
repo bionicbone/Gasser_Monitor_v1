d10b99bba1a9b897677b0dd90a3d83e89fe40602
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2020-05-24 07:01:15 +0100

    Telemetry - constexpr auto instead of #define in .h files
    constexpr auto instead of #define in .h files

--- Telemetry.cpp
+++ Telemetry.cpp
@@ -65,16 +65,6 @@ TODO List:-
 #include "Temperature.h"
 #include "Power.h"
 
-// Config
-#define SPORT_START 0x7E											// FrSky Start frame indicator
-#define SPORT_HEADER_DATA 0x10								// FrSky Data frame indicator
-#define SENSOR_GASSER_ID 0xF2									// (0xF2 on SPort Bus)
-#define SPORT_HEADER_NODATA 0x00							// Signal no refresh required
-#define SENSOR_FLVSS_ID 0xA1									// (0xA1 on SPort Bus)
-#define SENSOR_FLVSS_DECODE_DELAY 680					// 680 allows a 708ms decode of the FLVSS data while ensuring the encoding rates of the telemetry data is not impacted
-constexpr auto SMOOTH_MULTIPLIER = 0.25;			// used to smooth the cell value, 0.1 maxmum smoothing and 0.1 minimum smoothing
-constexpr auto SMOOTH_OVER_RIDE = 0.3;				// Over rides the smooth function and follow new value istantly if the change is more than this
-
 // Public Variables
 float					cell[6] = { 0.0000 };								// Final cell voltage store
 float cellSmoothed[6] = { 0.0000 };								// Final cell voltage after smoothing
