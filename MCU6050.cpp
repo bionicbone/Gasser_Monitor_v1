05bfde264c4fa69509edc39e17a991a96c422edc
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2020-06-28 11:49:59 +0100

    Vibration public variables
    Make key variables public so they can be written to SD Card logs
    Also renamed the variables a little so they match the SD card log headers

--- MCU6050.cpp
+++ MCU6050.cpp
@@ -6,10 +6,14 @@
 #include <Wire.h>
 extern TwoWire Wire1;
 
+// Public Variables
+int _AccX, _AccY, _AccZ, _AccTmp, _GyrX, _GyrY, _GyrZ;	// MCU-6050 values
+byte _vibrationStatus = 0;															// MCU-6050 status based on values, 0 = Low to zero vibration, 1 = Wake Up OLED, 2 = Engine Running, 3 = High Vibration, 4 = Extreme Vibration
+
+
 // Private Variables
-int AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;					// MCU-6050 values
-int avgAcX, avgAcY, avgAcZ, avgTmp;							// MCU-6050 are calcualted over several readings (50 hard coded)
-int avgGyX, avgGyY, avgGyZ;											// MCU-6050 are calcualted over several readings (50 hard coded)
+int avgAccX, avgAccY, avgAccZ, avgTmp;							// MCU-6050 are calcualted over several readings (50 hard coded)
+int avgGyrX, avgGyrY, avgGyrZ;											// MCU-6050 are calcualted over several readings (50 hard coded)
 int MCU6050Readings = 0;												// Number of MCU-6050 readings for calculating the average
 int MCU6050WakeCounter = 0;											// Number of times one of the MCU-6050 WAKE readings is more then the allowed difference in a row
 int MCU6050EngineCounter = 0;										// Number of times one of the MCU-6050 ENGINE readings is more then the allowed difference in a row
@@ -36,42 +40,44 @@ void _mcu6050_Read() {
 	Wire1.endTransmission(false);
 
 	Wire1.requestFrom(MPU_addr, 14, true);  // request a total of 14 registers
-	avgAcX += Wire1.read() << 8 | Wire1.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-	avgAcY += Wire1.read() << 8 | Wire1.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-	avgAcZ += Wire1.read() << 8 | Wire1.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+	avgAccX += Wire1.read() << 8 | Wire1.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+	avgAccY += Wire1.read() << 8 | Wire1.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+	avgAccZ += Wire1.read() << 8 | Wire1.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 	avgTmp += Wire1.read() << 8 | Wire1.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-	avgGyX += Wire1.read() << 8 | Wire1.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-	avgGyY += Wire1.read() << 8 | Wire1.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-	avgGyZ += Wire1.read() << 8 | Wire1.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+	avgGyrX += Wire1.read() << 8 | Wire1.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+	avgGyrY += Wire1.read() << 8 | Wire1.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+	avgGyrZ += Wire1.read() << 8 | Wire1.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 	MCU6050Readings++;
 
 	if (MCU6050Readings > 50) {
-		int AcX_Diff = AcX - (avgAcX / MCU6050Readings);
-		int AcY_Diff = AcY - (avgAcY / MCU6050Readings);
-		int AcZ_Diff = AcZ - (avgAcZ / MCU6050Readings);
+		int AccX_Diff = _AccX - (avgAccX / MCU6050Readings);
+		int AccY_Diff = _AccY - (avgAccY / MCU6050Readings);
+		int AccZ_Diff = _AccZ - (avgAccZ / MCU6050Readings);
+		_vibrationStatus = 0;
 
-		if (AcX && AcY && AcZ) {  // Safe guards the initial run of the code
+		if (_AccX && _AccY && _AccZ) {  // Safe guards the initial run of the code
 			// Work out if the Diff has been Breached at wake up level
-			if ((abs(AcX_Diff) >= MCU_WAKE_AcX_ALLOWED_DIFF
-				&& abs(AcX_Diff) < MCU_ENGINE_AcX_ALLOWED_DIFF)
-				|| (abs(AcY_Diff) >= MCU_WAKE_AcY_ALLOWED_DIFF
-					&& abs(AcY_Diff) < MCU_ENGINE_AcY_ALLOWED_DIFF)
-				|| (abs(AcZ_Diff) >= MCU_WAKE_AcZ_ALLOWED_DIFF
-					&& abs(AcZ_Diff) < MCU_ENGINE_AcZ_ALLOWED_DIFF)) {
+			if ((abs(AccX_Diff) >= MCU_WAKE_AccX_ALLOWED_DIFF
+				&& abs(AccX_Diff) < MCU_ENGINE_AccX_ALLOWED_DIFF)
+				|| (abs(AccY_Diff) >= MCU_WAKE_AccY_ALLOWED_DIFF
+					&& abs(AccY_Diff) < MCU_ENGINE_AccY_ALLOWED_DIFF)
+				|| (abs(AccZ_Diff) >= MCU_WAKE_AccZ_ALLOWED_DIFF
+					&& abs(AccZ_Diff) < MCU_ENGINE_AccZ_ALLOWED_DIFF)) {
 				MCU6050WakeCounter++;
 			}
 			else {
 				MCU6050WakeCounter = 0;
 			}
-			if (abs(AcX_Diff) >= MCU_ENGINE_AcX_ALLOWED_DIFF
-				|| abs(AcY_Diff) >= MCU_ENGINE_AcY_ALLOWED_DIFF
-				|| abs(AcZ_Diff) >= MCU_ENGINE_AcZ_ALLOWED_DIFF) {
+			if (abs(AccX_Diff) >= MCU_ENGINE_AccX_ALLOWED_DIFF
+				|| abs(AccY_Diff) >= MCU_ENGINE_AccY_ALLOWED_DIFF
+				|| abs(AccZ_Diff) >= MCU_ENGINE_AccZ_ALLOWED_DIFF) {
 				MCU6050EngineCounter++;
 			}
 			else {
 				MCU6050EngineCounter = 0;
 			}
 		}
+		
 		// Do I need to report?
 		if (millis() > MCU_STABILITY_DELAY) { // only perform updates once stable
 			// Do we need to wake up?
@@ -79,28 +85,30 @@ void _mcu6050_Read() {
 				wakeUpStartTimerMS = millis();
 				wakeUp = true;
 				wakeUpNow = true;
+				_vibrationStatus = 1;
 				Serial.println("Wake Up...");
-				Serial.print("AcX Diff = "); Serial.println(abs(AcX_Diff));
-				Serial.print("AcY Diff = "); Serial.println(abs(AcY_Diff));
-				Serial.print("AcZ Diff = "); Serial.println(abs(AcZ_Diff));
+				Serial.print("AcX Diff = "); Serial.println(abs(AccX_Diff));
+				Serial.print("AcY Diff = "); Serial.println(abs(AccY_Diff));
+				Serial.print("AcZ Diff = "); Serial.println(abs(AccZ_Diff));
 			}
 			else if (MCU6050EngineCounter >= 100) {
 				wakeUp = false;
+				_vibrationStatus = 2;
 				Serial.println("Engine Running...");
-				Serial.print("AcX Diff = "); Serial.println(abs(AcX_Diff));
-				Serial.print("AcY Diff = "); Serial.println(abs(AcY_Diff));
-				Serial.print("AcZ Diff = "); Serial.println(abs(AcZ_Diff));
+				Serial.print("AcX Diff = "); Serial.println(abs(AccX_Diff));
+				Serial.print("AcY Diff = "); Serial.println(abs(AccY_Diff));
+				Serial.print("AcZ Diff = "); Serial.println(abs(AccZ_Diff));
 			}
 
 
-			AcX = avgAcX / MCU6050Readings;
-			AcY = avgAcY / MCU6050Readings;
-			AcZ = avgAcZ / MCU6050Readings;
-			Tmp = avgTmp / MCU6050Readings;
-			Tmp = Tmp / 340.00 + 36.53;
-			GyX = avgGyX / MCU6050Readings;
-			GyY = avgGyY / MCU6050Readings;
-			GyZ = avgGyZ / MCU6050Readings;
+			_AccX = avgAccX / MCU6050Readings;
+			_AccY = avgAccY / MCU6050Readings;
+			_AccZ = avgAccZ / MCU6050Readings;
+			_AccTmp = avgTmp / MCU6050Readings;
+			_AccTmp = _AccTmp / 340.00 + 36.53;
+			_GyrX = avgGyrX / MCU6050Readings;
+			_GyrY = avgGyrY / MCU6050Readings;
+			_GyrZ = avgGyrZ / MCU6050Readings;
 
 
 			// TODO - Add Calibration
@@ -108,22 +116,22 @@ void _mcu6050_Read() {
 		}
 
 		MCU6050Readings = 0;
-		avgAcX = 0;
-		avgAcY = 0;
-		avgAcZ = 0;
+		avgAccX = 0;
+		avgAccY = 0;
+		avgAccZ = 0;
 		avgTmp = 0;
-		avgGyX = 0;
-		avgGyY = 0;
-		avgGyZ = 0;
+		avgGyrX = 0;
+		avgGyrY = 0;
+		avgGyrZ = 0;
 
 
-		//Serial.print("AcX = "); Serial.print(AcX);
-		//Serial.print(" | AcY = "); Serial.print(AcY);
-		//Serial.print(" | AcZ = "); Serial.print(AcZ);
-		//Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53);  //equation for temperature in degrees C from datasheet
-		//Serial.print(" | GyX = "); Serial.print(GyX);
-		//Serial.print(" | GyY = "); Serial.print(GyY);
-		//Serial.print(" | GyZ = "); Serial.println(GyZ);
+		//Serial.print("AccX = "); Serial.print(_AccX);
+		//Serial.print(" | AccY = "); Serial.print(_AccY);
+		//Serial.print(" | AccZ = "); Serial.print(_AccZ);
+		//Serial.print(" | AccTmp = "); Serial.print(_Tmp); 
+		//Serial.print(" | GyrX = "); Serial.print(_GyrX);
+		//Serial.print(" | GyrY = "); Serial.print(_GyrY);
+		//Serial.print(" | GyrZ = "); Serial.println(_GyrZ);
 	}
 
 }
